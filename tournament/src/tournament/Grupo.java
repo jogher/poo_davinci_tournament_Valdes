e84f8eb5c55809beb3f8047419de5913a8e8319c
Sergio Medina <63879459+sergiomedinaio@users.noreply.github.com>
2023-06-24 14:24:33 -0300

    fix: rename ArrayList

--- tournament/src/tournament/Grupo.java
+++ tournament/src/tournament/Grupo.java
@@ -1,7 +1,7 @@
 package tournament;
 
 public class Grupo extends Season {
-    public arrayList<Team> getEquiposQueAvanzan(){
+    public ArrayList<Team> getEquiposQueAvanzan(){
         return teams;
     };
 
